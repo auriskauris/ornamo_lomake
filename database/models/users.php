01ca7258a9de122312864a9e612c2831b13b30d2
Tinkle hole <anniina.nuutinen@edu.tampere.fi>
2022-09-07 14:21:20 +0300

    cleaners.php lisätty (myös indexiin)

---
+++ database/models/users.php
@@ -0,0 +1,23 @@
+<?php
+require_once "database/connection.php";
+
+// Tänne functiot jotka lisää henkilön, hakee alat jne
+
+// esim. 
+
+// function getAllSchools(){
+//     $pdo =connectDB();
+//     $sql = "SELECT * FROM school";
+//     $stm = $pdo->query($sql);
+//     $all = $stm->fetchAll(PDO::FETCH_ASSOC);
+//     return $all;
+// }
+
+// function addUser($username, $password, $email){
+//     $pdo = connectDB();
+//     $dbpassword = hashPassword($password);
+//     $data = [$username, $dbpassword, $email];
+//     $sql = "INSERT INTO user (username, password, email, approved) VALUES(?,?,?,'0')";
+//     $stm=$pdo->prepare($sql);
+//     return $stm->execute($data);
+// }
